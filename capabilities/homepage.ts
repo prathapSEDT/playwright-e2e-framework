800d9d8b88e44a378923a2dc8ee55f9e1fb26fed
ashok <Ashokmajetiit@gmail.com>
2025-05-28 08:16:00 +0530

    modified weblib

--- capabilities/homepage.ts
+++ capabilities/homepage.ts
@@ -1,10 +1,11 @@
-import { Locator } from "@playwright/test"
+import homePageLocator from '../locators/homepage.json'
+import WebLib from "../utils/webutils/webutils";
 
-export default class Homepage{
- async navigateToRegistrationPage()  {
-console.log("navigate to registration page")
- }
-
- 
-  
+export default class Homepage extends WebLib {
+    async navigateToRegistrationPage() {
+        await this.clickElement(homePageLocator['register'])
+    }
+    async navigateToLoginPage() {
+        await this.clickElement(homePageLocator['login'])
+    }
 }
